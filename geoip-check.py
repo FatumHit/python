3a82a30a6e2fae07eea8b4e848e3739355d21a0d
FatumHit <71377942+FatumHit@users.noreply.github.com>
2020-12-22 09:23:25 +0200

    Create geoip-check.py

---
+++ geoip-check.py
@@ -0,0 +1,43 @@
+#!/opt/rh/rh-python36/root/usr/bin/python3.6
+
+"""
+SCript Usage :
+run from command line : ./geoip-check.py 8.8.8.8 (replace by any IP you want to check)
+you will receive results from 8 separate DB's
+"""
+from sys import argv
+import urllib.request
+import urllib.parse
+import json
+
+
+pars = argv[1]
+
+
+
+
+def form(ip_addr):
+  link1 = "https://ipinfo.io/"+ip_addr+"/geo"
+  link2 = "http://free.ipwhois.io/json/"+ip_addr
+  link4 = "http://ip-api.com/json/"+ip_addr
+  link5 = "https://extreme-ip-lookup.com/json/"+ip_addr
+  links = [link1, link2, link4, link5]
+  print("\nLocation for IP Address : "+ip_addr)
+
+  for i in links:
+    url = urllib.request.urlopen(i)
+    url_list = url.read().decode('utf-8')
+    json_loaded = json.loads(url_list)
+    #print(json_loaded)
+    print("\n                   ............................")
+    print ("                    City    : "+ json_loaded['city'])
+    print ("                    Country    : "+ json_loaded['country'])
+
+  print("\n                ............................")
+
+  return()
+
+
+
+form(pars)
+~                 
